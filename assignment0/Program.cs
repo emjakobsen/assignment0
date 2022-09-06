37ebbdb250a01dfa3175659f7b1545fdc867dc11
erja <erja@itu.dk>
2022-09-06 13:39:52 +0200

    Exercise 3 and 4.

--- assignment0/Program.cs
+++ assignment0/Program.cs
@@ -1,2 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+namespace assignment0;
+class Program 
+{
+    static void Main (string[] args) {
+        //Console.WriteLine("Hello, World!");
+        
+    
+
+        var a0 = new Assignment0Class();
+        int input = Convert.ToInt32(Console.ReadLine());
+        
+        
+        
+        Console.WriteLine(a0.isLeapYear(input));
+    
+        // med dotnet run <dit nummer> -- 0
+        //Console.WriteLine(a0.isLeapYear(Int32.Parse(args[1])));
+    }
+    
+    
+
+}
